70b49259d495ecfbd32bca7164a4056eafc77fd8
Aman <rumman.djuice1@gmail.com>
2019-02-04 23:47:45 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import { routes } from "./routes"; //the { same }  from "./same"  means the name of the route array and its js fine have to be same
 
 Vue.use(VueRouter);
 
-const router=new VueRouter({});
+const router=new VueRouter({routes});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
