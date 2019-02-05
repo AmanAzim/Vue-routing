f25e6927d1b7dce95be001881300c03839e9bc06
Aman <rumman.djuice1@gmail.com>
2019-02-05 19:04:49 +0100

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
     <div>
         <ul class="nav nav-pills">
             <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
-            <router-link tag="li" active-class="active" to="/user/1"><a>User 1</a></router-link>
-            <router-link tag="li" active-class="active" to="/user/2"><a>User 2</a></router-link>
+            <router-link tag="li" active-class="active" to="/user"><a>User</a></router-link>
+
         </ul>
     </div>
 </template>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,8 +2,10 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <p>The loaded id: {{id}}</p>
+        <!-- <p>The loaded id: {{id}}</p> -->
         <button class="btn btn-primary" v-on:click="navigateToHome">Navigate to Home</button>
+        <br><br><hr>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -11,13 +13,13 @@
     export default{
         data(){
             return{
-                id:this.$route.params.id,
+               // id:this.$route.params.id,
             };
         },
         watch:{
-            '$route'(to, from){
+           /* '$route'(to, from){
                 this.id=to.params.id;
-            }
+            }*/
         },
         methods:{
             navigateToHome(){
